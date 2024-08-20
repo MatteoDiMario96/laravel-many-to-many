8f25fc9fe70c2c503283699082323b1027d32023
MatteoDiMario96 <dm.matteo96@live.it>
2024-08-20 21:06:33 +0200

    Aggiunto TechnologySeeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            TechnologySeeder::class,
             TypeSeeder::class,
             UserSeeder::class,
             ProjectSeeder::class,
